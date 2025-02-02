ed4226d9a7777012eb7f160561417ee68b5f889b
黄骥 <884358@qq.com>
2025-02-02 19:49:14 +0800

    init

---
+++ www/index.php
@@ -0,0 +1,30 @@
+<html>
+ <head>
+  <title>Hello...</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    <?php echo "<h1>Hello,docker!</h1>"; ?>
+    <?php
+    $conn = mysqli_connect('db', 'test', 'test', "testdb");
+    $query = 'SELECT * From person';
+    $result = mysqli_query($conn, $query);
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+    while($value = $result->fetch_array(MYSQLI_ASSOC)){
+        echo '<tr>';
+        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+        foreach($value as $element){
+            echo '<td>' . $element . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+    $result->close();
+    mysqli_close($conn);
+    ?>
+    </div>
+</body>
+</html>
